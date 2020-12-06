2aa239a9a7aa2fd88d6c461177db482c565bf0ec
genzisan <73005681+genzisan@users.noreply.github.com>
2020-12-06 16:36:37 +0900

    初めてにコミット

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  // showメソッドを用いて、「#title」要素を表示してください
+  $('#title').show();
+  
+  // fadeInメソッドを用いて、「#image」要素を表示してください
+  
+  $("#image1").fadeIn(2000);
+  $("#image2").fadeIn(4000);
+  $("#image3").fadeIn(6000);
+  $("#image4").fadeIn(8000);
+});
+
